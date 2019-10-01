2a5ea805984f1580303f7259a523818aaf0d9150
Evan Jeffries <ejeffries14@gmail.com>
2019-10-01 13:58:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  @@account = []
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+    
+    @@account << self
+  end
+  
+  def deposit
+    @@account << @balance
+  end
+  
+  def display_balance
+    puts "Your balance is #{deposit}."
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
+  attr_accessor :sender, :receiver, :status, :amount
   # your code here
+  def initialize(sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+  
+  def amount
+    @amount
+  end
+  
+  def valid?(sender, receiver)
+    if sender || receiver
+      puts "valid"
+    end
+  end
 end
